e94618dd347dfb54f57acbb5dc1337b971263154
Minh Dau <mtcd001@bucknell.edu>
2026-07-14 01:27:49 +0700

    Created Admin Dashboard

---
+++ admin/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {assets} from "../assets/assets.js";
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center px-5 py-3 sm:px-10 sm:py-5 bg-gray-100">
+      <img className = 'w-[max(10%,80px)]' src={assets.logo} alt="" />
+      <button className = 'bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
+  );
+};
+
+export default NavBar;
