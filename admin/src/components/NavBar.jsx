2e7b4aad28fd23f9130dfdb48e56fbdde772a23e
Minh Dau <mtcd001@bucknell.edu>
2026-07-25 00:21:47 -0400

    Connect Backend APIs with Admin panel

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {assets} from "../assets/assets.js";
-const NavBar = () => {
+const NavBar = ({setToken}) => {
   return (
     <div className="flex justify-between items-center px-5 py-3 sm:px-10 sm:py-5 bg-gray-100">
       <img className = 'w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button className = 'bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+      <button onClick = {() => setToken('')} className = 'bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   );
 };
