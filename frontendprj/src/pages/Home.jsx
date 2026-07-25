151fbe91ed2b95866fca43ab020ee295fe3b5367
Minh Dau <mtcd001@bucknell.edu>
2026-07-25 02:27:35 -0400

    Finish backend

--- frontendprj/src/pages/Home.jsx
+++ frontendprj/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestItem from '../components/LatestItem'
+import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import Footer from '../components/Footer'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
     <div>
-        <Hero/> 
-        <LatestItem/>
-        <OurPolicy/>
-        <Footer/>
+      <Hero />
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
