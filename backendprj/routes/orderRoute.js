063f86a8c5b0d596d31de798008c3b11fba24d27
Minh Dau <mtcd001@bucknell.edu>
2026-07-27 23:15:42 -0400

    finish

--- backendprj/routes/orderRoute.js
+++ backendprj/routes/orderRoute.js
@@ -1,15 +1,22 @@
-import express from "express";
+import express from 'express'
+import {placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus, verifyStripe} from '../controllers/orderController.js'
+import adminAuth  from '../middleware/adminAuth.js'
+import authUser from '../middleware/auth.js'
 
-import { placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus } from "../controllers/orderController.js";
-import adminAuth from "../middlewares/adminAuth.js";
-import authUser from "../middlewares/auth.js";
+const orderRouter = express.Router()
 
-const orderRouter = express.Router();
+// Admin Features
+orderRouter.post('/list',adminAuth,allOrders)
+orderRouter.post('/status',adminAuth,updateStatus)
 
-orderRouter.post("/place", authUser, placeOrder)
-orderRouter.post("/stripe", authUser, placeOrderStripe)
-orderRouter.post("/list",adminAuth, allOrders)
-orderRouter.post("/userorders", authUser, userOrders)
-orderRouter.post("/status",adminAuth, updateStatus)
+// Payment Features
+orderRouter.post('/place',authUser,placeOrder)
+orderRouter.post('/stripe',authUser,placeOrderStripe)
+
+// User Feature 
+orderRouter.post('/userorders',authUser,userOrders)
+
+// verify payment
+orderRouter.post('/verifyStripe',authUser, verifyStripe)
 
 export default orderRouter
